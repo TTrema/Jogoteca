8588803aa86545ec4604f324fc58c04cd0eee7f0
jwill <114110689+TTrema@users.noreply.github.com>
2022-11-24 02:04:56 -0300

    primeira build

---
+++ README.md
@@ -0,0 +1,3 @@
+﻿Um site de lista de jogos feito em Flask com base no curso:
+
+https://cursos.alura.com.br/formacao-flask
---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+SECRET_KEY = "alura"
+
+SQLALCHEMY_DATABASE_URI = "{SGBD}://{usuario}:{senha}@{servidor}/{database}".format(
+    SGBD="mysql+mysqlconnector", usuario="root", senha="admin", servidor="127.0.0.1", database="jogoteca"
+)
+
+UPLOAD_PATH = os.path.dirname(os.path.abspath(__file__)) + "/uploads"
